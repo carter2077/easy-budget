ba134cf2c842cb11209c826e87c6bdf0203549fe
carter2077 <carter2077@pm.me>
2024-04-01 18:47:40 -0400

    Init spring boot

---
+++ src/main/java/com/carter2077/easyfinance/EasyFinanceApplication.java
@@ -0,0 +1,13 @@
+package com.carter2077.easyfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyFinanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyFinanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/carter2077/easyfinance/EasyFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carter2077.easyfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyFinanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
