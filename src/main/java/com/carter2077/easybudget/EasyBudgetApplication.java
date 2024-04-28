b07829cef86e63260d67b6644422165c159fd51c
carter2077 <carter2077@pm.me>
2024-04-28 13:43:20 -0400

    back to spring boot

---
+++ src/main/java/com/carter2077/easybudget/EasyBudgetApplication.java
@@ -0,0 +1,13 @@
+package com.carter2077.easybudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyBudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyBudgetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carter2077/easybudget/EasyBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carter2077.easybudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyBudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
